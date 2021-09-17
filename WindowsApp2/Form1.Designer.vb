95a70b96753b05234f946b0b6c51bd6dad654432
William Perkett <perkettbill@gmail.com>
2021-09-17 08:50:42 -0400

    Add files via upload

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnHello = New System.Windows.Forms.Button()
+        Me.BtnEnd = New System.Windows.Forms.Button()
+        Me.LblHello = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnHello
+        '
+        Me.BtnHello.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnHello.Location = New System.Drawing.Point(94, 114)
+        Me.BtnHello.Name = "BtnHello"
+        Me.BtnHello.Size = New System.Drawing.Size(75, 31)
+        Me.BtnHello.TabIndex = 0
+        Me.BtnHello.Text = "Hello"
+        Me.BtnHello.UseVisualStyleBackColor = True
+        '
+        'BtnEnd
+        '
+        Me.BtnEnd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEnd.Location = New System.Drawing.Point(542, 114)
+        Me.BtnEnd.Name = "BtnEnd"
+        Me.BtnEnd.Size = New System.Drawing.Size(75, 31)
+        Me.BtnEnd.TabIndex = 1
+        Me.BtnEnd.Text = "End"
+        Me.BtnEnd.UseVisualStyleBackColor = True
+        '
+        'LblHello
+        '
+        Me.LblHello.AutoSize = True
+        Me.LblHello.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblHello.Location = New System.Drawing.Point(308, 119)
+        Me.LblHello.Name = "LblHello"
+        Me.LblHello.Size = New System.Drawing.Size(0, 20)
+        Me.LblHello.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LblHello)
+        Me.Controls.Add(Me.BtnEnd)
+        Me.Controls.Add(Me.BtnHello)
+        Me.Name = "Form1"
+        Me.Text = "Hello World "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnHello As Button
+    Friend WithEvents BtnEnd As Button
+    Friend WithEvents LblHello As Label
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub BtnHello_Click(sender As Object, e As EventArgs) Handles BtnHello.Click
+        LblHello.Text = "Hello World"    ' Write Hello World
+        LblHello.BackColor = Color.Aqua  ' Background Aqua
+        LblHello.ForeColor = Color.Red   ' Text Red
+    End Sub
+
+    Private Sub BtnEnd_Click(sender As Object, e As EventArgs) Handles BtnEnd.Click
+        End  ' End program without using Blue Square on Menu
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
